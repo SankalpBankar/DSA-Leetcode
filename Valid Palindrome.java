73d18331e09be748db618e9172b8e1b7976d41e3
SankalpBankar <sankalpbankar7@gmail.com>
2026-03-04 23:24:26 +0530

    Cleaned up unnecessary files

--- Valid Palindrome.java
+++
@@ -1,7 +0,0 @@
-class Solution {
-    public boolean isPalindrome(String s) {
-        String cleaned = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        String reversed = new StringBuilder(cleaned).reverse().toString();
-        return cleaned.equals(reversed);
-    }
-}
