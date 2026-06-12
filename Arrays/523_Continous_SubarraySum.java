fc3fadf650993a65c287dc918ff06fb637129171
SankalpBankar <sankalpbankar7@gmail.com>
2026-06-12 23:36:20 +0530

    Solution added

---
+++ Arrays/523_Continous_SubarraySum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, -1); // handles subarrays starting from index 0
+
+        int prefixSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            prefixSum += nums[i];
+
+            int remainder = prefixSum % k;
+
+            if (map.containsKey(remainder)) {
+                if (i - map.get(remainder) >= 2) {
+                    return true;
+                }
+            } else {
+                map.put(remainder, i);
+            }
+        }
+
+        return false;
+    }
+}
